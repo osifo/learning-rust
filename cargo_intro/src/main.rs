1ae5a9f2e1cc39e79e1c68db33b7295332b6d8a7
Osifo Anosike <code.anosikeosifo@gmail.com>
2020-06-22 05:06:17 +0100

    chapter 1

---
+++ cargo_intro/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Hello, world!");//the invocation with the bang indicates that we are calling a MACRO and not a FUNCTION
+}
---
+++ intro/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+  println!("Welcome to programming in rust");
+}
