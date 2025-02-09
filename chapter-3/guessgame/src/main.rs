1ccb8aaf5062b780653534412779628e541a96a3
Anosike Osifo <code.anosikeosifo@gmail.com>
2025-02-09 02:33:10 +0100

    complete chapter 2

--- chapter-3/guessgame/src/main.rs
+++ chapter-3/guessgame/src/main.rs
@@ -3,31 +3,35 @@ use std::io::stdin;
 use std::cmp::Ordering;
 
 fn guessing_game() -> () {
-    let rand_number: i32 = rand::rng().random_range(1..=100);
-    let mut game_over = false;
+    let rand_number = rand::rng().random_range(1..=100);
+        
+    println!("A number has been generated");
+    println!("guess the number");
 
-    println!("{}", rand_number);
-
-    println!("enter a number");
-
-    while !game_over {
+    
+    loop {
         let mut user_guess = String::new();
         stdin()
             .read_line(&mut user_guess)
             .expect("Failed to read line");
 
-        let user_guess: i32 = user_guess.trim().parse().expect("Please enter a number.");
+        let user_guess: u32 = match user_guess.trim().parse() {
+            Ok(input) => input,
+            Err(_) => {
+                println!("guess the number");
+                continue;
+            }, //moves to the next iteration of the loop
+        };
     
         let result_message = match user_guess.cmp(&rand_number) {
             Ordering::Less => "Guess a higher number",
             Ordering::Greater => "Guess a lower number",
-            Ordering::Equal => {
-                game_over = true;
-                "Correct!"
-            }
+            Ordering::Equal => "Correct!"
         };
         
-        println!("{}", result_message)
+        println!("{}", result_message);
+
+        if result_message == "Correct!" { break; };
     }
 }
 
